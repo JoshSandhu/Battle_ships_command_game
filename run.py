824680b6723d3a750610934b8461547647c3a0e1
JoshSandhu <joshsandhulindholm@gmail.com>
2022-04-27 11:50:52 +0000

    Added code to generate the board for play

--- run.py
+++ run.py
@@ -1,1 +1,18 @@
+from random import randint
+
+user = []
+user_guesses = []
+computer = []
+
+
+def gen_board(board):
+    """
+    Creating a board of 5 "X" in 5 lists
+    Arguments: Empty list
+    Return: list with 5 lists of the letter X
+    """
+    for i in range(5):
+        board.append(["X"] * 5)
+    return board
+
 
