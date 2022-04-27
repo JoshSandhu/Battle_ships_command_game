83e08cc1dd0489fb7df1c6e3338b540138f6bf10
JoshSandhu <joshsandhulindholm@gmail.com>
2022-04-27 12:03:18 +0000

    Added code for random number generation for the board

--- run.py
+++ run.py
@@ -19,9 +19,18 @@ def gen_board(board):
 def print_board(board):
     """
     Prints the board list of X,
-    Removing the lists formatting with spaces
+    Replacing the lists formatting with spaces
     Argument: List
     """
     for ind in board:
         print(" ".join(ind))
+
+
+def random_num(board):
+    """
+    Generate the random number between 0 and the length of the board(Minus one)
+    Argument: List
+    """
+    return randint(0, len(board) - 1)
+
 
